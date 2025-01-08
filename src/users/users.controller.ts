b80cf7b5ea46ead782b6f5c83fb8ef9191deea11
astonmedia <astonmedia@hotmail.com>
2025-01-08 17:43:48 +1000

    Update from PC 080125

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,14 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -19,8 +21,12 @@ export class UsersController {
   }
 
   @Get('/users/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   @Get('/users')
@@ -29,8 +35,8 @@ export class UsersController {
   }
 
   @Patch('/users/:id')
-  updateUser(@Param('id') id: number, @Body() body: CreateUserDto) {
-    return this.usersService.update(id, body);
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 
   @Delete('/users/:id')
