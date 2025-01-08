af213c8af393d7b446981c4382aea79bebb4b407
astonmedia <astonmedia@hotmail.com>
2025-01-09 06:21:10 +1000

    Update from laptop 090125. Interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,8 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user-dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -20,6 +22,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @Serialize(UserDto)
   @Get('/users/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
@@ -29,6 +32,7 @@ export class UsersController {
     return user;
   }
 
+  @Serialize(UserDto)
   @Get('/users')
   findAllUsers() {
     return this.usersService.find();
